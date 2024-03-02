9a3a6cd31a2b8d4467decfd36955d3fcd1dbd492
YutaNakamura <yu.nakamura@zenplace.co.jp>
2024-03-02 21:34:17 +0900

    fix: btn style

--- src/_shared/classnames.ts
+++ src/_shared/classnames.ts
@@ -1,3 +1,5 @@
 export const classnames = {
-  btn: `text-white bg-gray-800 hover:bg-gray-900 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700`
+  btn: `text-white bg-gray-800 hover:bg-gray-900 
+    focus:outline-none font-medium rounded-lg text-sm
+    px-5 py-2 me-2 mb-2`
 }
